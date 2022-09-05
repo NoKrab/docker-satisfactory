ba5c70f7a229c1cd07c729bedac2ab4bffe04b3c
BitSFitR <raymond.mao@protonmail.com>
2022-09-06 01:39:24 +0200

    update script

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@ export InstallationDir=/home/steam/SatisfactoryDedicatedServer
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$InstallationDir/linux64:$LD_LIBRARY_PATH
 # Install or update the server before launching it
-/usr/games/steamcmd +login anonymous +force_install_dir $InstallationDir +app_update 1690800 validate +quit
+/usr/games/steamcmd +force_install_dir $InstallationDir +login anonymous +app_update 1690800 -beta experimental validate +quit
 # Launch the server
 $InstallationDir/FactoryServer.sh
 export LD_LIBRARY_PATH=$templdpath
